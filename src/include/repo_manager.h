c7964a8f62f3f018a66945174c850464134afa0d
evverenn <jokerj224@gmail.com>
2023-04-25 17:58:00 +0300

    develop. Added github repo search by keyword.

--- src/include/repo_manager.h
+++ src/include/repo_manager.h
@@ -1,9 +1,19 @@
 #pragma once
 #include <string>
 #include <vector>
+
+#include "httplib.hpp"
 using std::string;
 using std::vector;
+typedef vector<string> RepoURLs;
 class RepoManager {
  private:
+  const string token{"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"};
+  const string apiurl{"https://api.github.com"};
+  const string searchRepoApiUrl{"/search/repositories"};
+  httplib::Client cli;
+
  public:
+  RepoManager();
+  RepoURLs FetchRepoUrls(string keyword);
 };
--- src/src/repo_manager.cc
+++ src/src/repo_manager.cc
@@ -1,1 +1,28 @@
+#define CPPHTTPLIB_OPENSSL_SUPPORT
 #include "repo_manager.h"
+
+#include "iostream"
+#include "json.hpp"
+
+using json = nlohmann::json;
+
+RepoManager::RepoManager() : cli(apiurl) {
+  cli.set_bearer_token_auth(token);
+  cli.enable_server_certificate_verification(false);
+}
+RepoURLs RepoManager::FetchRepoUrls(string keyword) {
+  RepoURLs urls;
+  httplib::Params params = {{"q", keyword}};
+  httplib::Headers headers = {{"Accept", "application/vnd.github+json"}};
+  try {
+    auto res = cli.Get(searchRepoApiUrl, params, headers);
+    json responseJSON = json::parse(res->body);
+    json items = responseJSON["items"];
+    for (auto& repo : items) {
+      urls.push_back(repo["url"]);
+    }
+  } catch (const std::exception& e) {
+    std::cout << "Response Error:" << e.what() << "\n";
+  }
+  return urls;
+}

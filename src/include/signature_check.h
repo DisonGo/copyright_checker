a7e6cb85b3aecb76c50a58aa1545da2dffc80eed
burnsdov <fable1336@gmail.com>
2023-04-25 21:45:48 +0300

    develop. Style fixed.

--- src/include/signature_check.h
+++ src/include/signature_check.h
@@ -1,17 +1,21 @@
 #ifndef SRC_INCLUDE_SIGNATURE_CHECK_H
 #define SRC_INCLUDE_SIGNATURE_CHECK_H
 
-#include <iostream>
 #include <fstream>
+#include <iostream>
 #include <string>
 #include <vector>
 
-int GetSignatureInfo(std::string reference_file, std::string checked_file);
-double GetMatchedPercentage(std::vector<std::string>& ref, std::vector<std::string>& copy);
+int GetSignatureInfo(const std::string& reference_file, const std::string& checked_file);
+double GetMatchedPercentage(std::vector<std::string>& ref,
+                            std::vector<std::string>& copy);
 std::vector<std::string> SignatureNormalize(std::ifstream& input_file);
 void RemoveExtra(std::string& str);
 void RemoveQuotes(std::string& str);
 void RemoveVariables(std::vector<std::string>& strings);
-void RemoveVariableFromString(std::vector<std::string>& str, std::vector<std::string>& variable_names);
+void ReadVariableName(std::string& from, std::string& buffer, size_t& pos);
+inline bool IsEndOfName(const char& current);
+void RemoveVariableFromString(std::vector<std::string>& str,
+                              std::vector<std::string>& variable_names);
 
 #endif  //  SRC_INCLUDE_SIGNATURE_CHECK_H

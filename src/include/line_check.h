d6fce981b2aeb122a2f3846dbf3c141aa33de6d3
burnsdov <fable1336@gmail.com>
2023-04-25 21:45:26 +0300

    develop. Deleted include of regex.

--- src/include/line_check.h
+++ src/include/line_check.h
@@ -1,13 +1,14 @@
 #ifndef SRC_INCLUDE_LINE_CHECK_H
 #define SRC_INCLUDE_LINE_CHECK_H
 
+#include <fstream>
 #include <iostream>
 #include <string>
-#include <fstream>
-#include <regex>
 
-int GetMatchPercentage(std::vector<std::string>& ref, std::vector<std::string>& check);
-int GetLineInfo(const std::string& reference_file, const std::string& checked_file);
+int GetMatchPercentage(std::vector<std::string>& ref,
+                       std::vector<std::string>& check);
+int GetLineInfo(const std::string& reference_file,
+                const std::string& checked_file);
 std::string NormalizeString(const std::string& str);
 inline bool IsForbidden(const char symbol);
 inline bool IsPreProcessing(const std::string& str);

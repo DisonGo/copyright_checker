bc1a71f21da9d61826fa0f533dfc580167d35afe
burnsdov <fable1336@gmail.com>
2023-04-28 10:38:50 +0300

    develop. Style fixed.

--- src/include/logger.h
+++ src/include/logger.h
@@ -6,6 +6,7 @@
 
 void InitLog(std::ofstream& log_file, std::string peer_name);
 void WriteResult(std::ofstream& log_file, size_t id, std::string git_link,
-                 int signature_percent, int line_percent, std::string file1, std::string file2);
+                 int signature_percent, int line_percent, std::string file1,
+                 std::string file2);
 void WriteMessage(std::ofstream& log_file, const std::string& msg);
 #endif  //  SRC_INCLUDE_LOGGER_H
